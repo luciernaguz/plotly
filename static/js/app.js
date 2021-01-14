6104a2039ec31972a287bee83f46a6cba1ae527f
luciernaguz <luciernaguz@hotmail.com>
2021-01-13 22:16:25 -0600

    Update structure

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+
+// The data from the JSON file is arbitrarily named importedData as the argument
+d3.json("data/samples.json").then((importedData) => {
+    // console.log(importedData);
+    var data = importedData;
+    console.log(importedData);
+    
+});
+
+//let id ="";
